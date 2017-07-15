6b6b3425add08acccee55240fb802c117f775a51
yitimo <635928008@qq.com>
2017-07-15 18:07:36 +0800

    import jsonpmodule in coremodule

--- src/app/-core/core.module.ts
+++ src/app/-core/core.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { HttpModule, JsonpModule } from '@angular/http';
 import { WeiboService } from './services/weibo.service';
 
 @NgModule({
     declarations: [],
-    imports: [ CommonModule, FormsModule, HttpModule ],
+    imports: [ CommonModule, FormsModule, HttpModule, JsonpModule ],
     exports: [],
     providers: [ WeiboService ],
 })
