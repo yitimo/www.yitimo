2d948430fa57f9f08edbf9d02278b19cfa1ca5d1
yitimo <635928008@qq.com>
2017-08-25 17:30:24 +0800

    remove weibo's refer

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -30,7 +30,7 @@ const ngcWebpack = require('ngc-webpack');
 const HMR = helpers.hasProcessFlag('hot');
 const AOT = process.env.BUILD_AOT || helpers.hasNpmFlag('aot');
 const METADATA = {
-  title: 'Yitibo!',
+  title: 'Yitimo!',
   baseUrl: '/',
   isDevServer: helpers.isWebpackDevServer(),
   HMR: HMR
