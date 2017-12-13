f811a3be003e34ff5e67f1ca86b5690b7c7b11f1
yitimo <admin@yitimo.com>
2017-12-13 11:37:58 +0800

    add line demo

--- src/app/pages/putsangto2/demo/demo.component.ts
+++ src/app/pages/putsangto2/demo/demo.component.ts
@@ -16,12 +16,27 @@ export class DemoComponent implements OnInit {
 
     public ngOnInit() {
         this.initScene();
+        this.initLight();
         this.initCamera();
         this.initRenderer();
         this.initDom('putsangto', this.renderer.domElement);
-        this.meshDemo(); // 旋转方块例子
+        this.lineDemo(); // 彩色线条例子
     }
 
+    private lineDemo() {
+        let geometry = new THREE.Geometry();
+        let material = new THREE.LineBasicMaterial({vertexColors: true});
+        let color = new THREE.Color(0x444444);
+        let color2 = new THREE.Color(0xFF0000);
+        let p1 = new THREE.Vector3(-100, 0, 100);
+        let p2 = new THREE.Vector3(100, 0, -100);
+        geometry.vertices.push(p1);
+        geometry.vertices.push(p2);
+        geometry.colors.push(color, color2);
+        let line = new THREE.Line(geometry, material, THREE.LineSegments);
+        this.scene.add(line);
+        this.renderer.render(this.scene, this.camera);
+    }
     private meshDemo() {
         let geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
         let material = new THREE.MeshNormalMaterial();
@@ -45,9 +60,24 @@ export class DemoComponent implements OnInit {
     private initScene() {
         this.scene = new THREE.Scene();
     }
+    private initLight() {
+        let light = new THREE.DirectionalLight(0xFF0000, 1.0, 0);
+        light.position.set(100, 100, 200);
+        this.scene.add(light);
+    }
     private initCamera() {
-        this.camera = new THREE.PerspectiveCamera( 70, 600 / 500, 0.01, 10 );
-        this.camera.position.z = 1;
+        this.camera = new THREE.PerspectiveCamera( 45, 600 / 500, 1, 10000 );
+        this.camera.position.x = 0;
+        this.camera.position.y = 1000;
+        this.camera.position.z = 0;
+        this.camera.up.x = 0;
+        this.camera.up.y = 0;
+        this.camera.up.z = 1;
+        this.camera.lookAt({
+            x : 0,
+            y : 0,
+            z : 0
+        });
     }
     private initRenderer() {
         this.renderer = new THREE.WebGLRenderer( { antialias: true } );
